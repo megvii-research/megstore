71c647889c401b39630da2ad91ecf83de7766f8c
penghongyang <phy654879998@foxmail.com>
2025-12-19 02:38:44 +0000

    chore:use pdm replace uv

--- megstore/__init__.py
+++ megstore/__init__.py
@@ -1,3 +1,4 @@
+from megstore.__version__ import __version__  # noqa: F401
 from megstore.indexed import (
     IndexedJsonlineReader,
     IndexedJsonlineWriter,
@@ -9,7 +10,6 @@ from megstore.indexed import (
     indexed_msgpack_open,
     indexed_txt_open,
 )
-from megstore.version import VERSION as __version__  # noqa: F401
 
 __all__ = [
     "IndexedJsonlineReader",
